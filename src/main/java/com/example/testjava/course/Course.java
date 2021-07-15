f662ebf57e65d1ccc749a0b11c83ad0ef04bea8f
puneetpapneja <puneetpapneja89@gmail.com>
2021-07-15 20:16:56 +0530

    Use sequence to generate courseID automatically

--- src/main/java/com/example/testjava/course/Course.java
+++ src/main/java/com/example/testjava/course/Course.java
@@ -1,11 +1,19 @@
 package com.example.testjava.course;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 
 @Entity
 public class Course {
     @Id
+    
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "seq_course")
+    @SequenceGenerator(sequenceName = "seq_course", allocationSize = 1, name = "seq_course")
+    //or 
+    //@GeneratedValue(strategy = GenerationType.SEQUENCE) by default use HIBERNATE_SEQUENCE 
     private Integer id;
 
     private String courseName;
